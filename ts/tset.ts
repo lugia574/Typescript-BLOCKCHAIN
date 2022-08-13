ddd6527f48d58c3dd5916f479bca9a805be94d53
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-08-13 12:36:54 +0900

    2022_0813

--- ts/tset.ts
+++ ts/tset.ts
@@ -1,1 +1,25 @@
-let b: boolean = "a";
+let b: boolean;
+
+b = false;
+
+let c: number[];
+
+class Person {
+  name: string;
+
+  constructor(name: string) {
+    // 클래스 프로퍼티의 선언과 초기화
+    this.name = name;
+  }
+}
+
+const player: {
+  name: string;
+  age?: number;
+} = {
+  name: "hahaha",
+};
+
+const num: readonly number[] = [1, 2, 3, 4];
+
+console.log(1 + num[0]);
